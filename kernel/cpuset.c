718b94319b74bf07d748c01d816c8933c2eda889
Zhao Hongjiang <zhaohongjiang@huawei.com>
2013-07-27 11:56:53 +0800

    cpuset: relocate a misplaced comment
    Comment for cpuset_css_offline() was on top of cpuset_css_free().
    Move it.
    
    Signed-off-by: Zhao Hongjiang <zhaohongjiang@huawei.com>
    Acked-by: Li Zefan <lizefan@huawei.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2028,6 +2028,12 @@ out_unlock:
 	return 0;
 }
 
+/*
+ * If the cpuset being removed has its flag 'sched_load_balance'
+ * enabled, then simulate turning sched_load_balance off, which
+ * will call rebuild_sched_domains_locked().
+ */
+
 static void cpuset_css_offline(struct cgroup *cgrp)
 {
 	struct cpuset *cs = cgroup_cs(cgrp);
@@ -2042,12 +2048,6 @@ static void cpuset_css_offline(struct cgroup *cgrp)
 
 	mutex_unlock(&cpuset_mutex);
 }
-
-/*
- * If the cpuset being removed has its flag 'sched_load_balance'
- * enabled, then simulate turning sched_load_balance off, which
- * will call rebuild_sched_domains_locked().
- */
 
 static void cpuset_css_free(struct cgroup *cgrp)
 {
