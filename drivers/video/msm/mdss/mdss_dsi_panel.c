3a0bdb19c4a205b46f04ebe9dfd88f20874cbb42
Steve Kondik <steve@cyngn.com>
2015-07-11 05:49:30 -0700

    video: mdss: Squashed support for LiveDisplay driver
    * video: mdss: LiveDisplay driver
      Change-Id: I4ed1f3876767dd6bedccaf0bbf61af7d36730ca0
    
    * video: mdss: Add new features and improve LiveDisplay
      Change-Id: I9b986593cf42b65a3360dae474d8318406b48204
    
    * video: mdss: Cache LiveDisplay RGB settings when display off
      Change-Id: I1d42842af0d044e241dcc5b9266be0db4f84800b
    
    * video: mdss: Fix dual-DSI issue in LiveDisplay
      Change-Id: I0d0c1475882eb65c8d5bc69c9ac3776b5d60339f
    
    * video: mdss: Implement PCC combining for LiveDisplay
      Change-Id: I33a5678a858f5e1c609709b4e7e901c792fd2590
    
    * video: mdss: Simplify PCC combiner
      Change-Id: I5e5b94ad117ec4c01d49fc38b1d967817de76825
    
    * video: mdss: Simplify PCC combiner even more
      Change-Id: I181a878d442e55e930c1a9532c40c84c3a9b1361
    
    * video: mdss: Fix issues when setting a channel to zero
      Change-Id: Idfaca981712abbf5bfeee9404525d8776bff1cb9
    
    Change-Id: I251dd1930566ff7067a63e354eb89048ce61e97c
    Signed-off-by: Quallenauge <Hamsi2k@Freenet.de>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -24,6 +24,7 @@
 
 #include "mdss_dsi.h"
 #include "mdss_dba_utils.h"
+#include "mdss_livedisplay.h"
 
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
@@ -156,8 +157,9 @@ u32 mdss_dsi_panel_cmd_read(struct mdss_dsi_ctrl_pdata *ctrl, char cmd0,
 	return 0;
 }
 
-static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds)
+
 {
 	struct dcs_cmd_req cmdreq;
 	struct mdss_panel_info *pinfo;
@@ -790,6 +792,8 @@ static int mdss_dsi_post_panel_on(struct mdss_panel_data *pdata)
 		mdss_dba_utils_hdcp_enable(pinfo->dba_data, true);
 	}
 
+	mdss_livedisplay_update(ctrl, MODE_UPDATE_ALL);
+
 end:
 	pr_debug("%s:-\n", __func__);
 	return 0;
@@ -873,7 +877,7 @@ static void mdss_dsi_parse_trigger(struct device_node *np, char *trigger,
 }
 
 
-static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
+int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 		struct dsi_panel_cmds *pcmds, char *cmd_key, char *link_key)
 {
 	const char *data;
@@ -2187,6 +2191,8 @@ static int mdss_panel_parse_dt(struct device_node *np,
 
 	pinfo->is_dba_panel = of_property_read_bool(np,
 			"qcom,dba-panel");
+
+	mdss_livedisplay_parse_dt(np, pinfo);
 
 	return 0;
 
