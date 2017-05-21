3c8ef41f4a90f9308564619d04b321004a77ca3b
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-05-21 19:01:53 +0200

    drivers: add MAXQ656 gpio driver from IIXOSOP5801607291S
    Change-Id: Ie98d59c6e4d9e2ba945961ab9ffc2b4b4514e108

---
+++ drivers/gpio/maxq656/maxq656.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2012 Samsung Electronics
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ */
+
+#ifndef _MAXQ656_H_
+#define _MAXQ656_H_
+
+struct ir_remocon_data {
+	struct mutex			mutex;
+	int maxq656_power_supply;
+	struct kobject *maxq656_obj;
+
+};
+#endif /* _IR_REMOTE_MAXQ656_H_ */
