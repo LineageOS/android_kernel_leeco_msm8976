00bfddf39fb0d95e07b05e72c1b47c6dccb541f3
Marios Thomos <marios199546@gmail.com>
2017-08-19 05:24:10 +0300

    cpu: Keep CPU core 4 always on
    Reduces wakeup time and overall speed with minimal increase in power consumption.
    Also we minimize the overhead which occurs every time that big cores are hot-plugging with each other.
    
    Change-Id: Ie129a50b8cca10259595e7699f35969b79ec1dcd

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -371,6 +371,13 @@ int __ref cpu_down(unsigned int cpu)
 
 	cpu_maps_update_begin();
 
+	// AP: Keep CPU core 4 always on
+	if (cpu == 4)
+	{
+		err = -EBUSY;
+		goto out;
+	}
+
 	if (cpu_hotplug_disabled) {
 		err = -EBUSY;
 		goto out;
