4b8c5d9cd4f96e3739400d6a0491bd59b6296750
Quallenauge <Hamsi2k@freenet.de>
2018-07-11 01:55:01 -0100

    wlan: increase MAX_CFG_INI_ITEMS
    Currently when the driver loads it logs an error that
    MAX_CFG_INI_ITEMS is too small.  So increase the value to compensate
    for the configuration items that have been recently added and to allow
    some future growth as well.

--- drivers/staging/prima/CORE/HDD/inc/wlan_hdd_cfg.h
+++ drivers/staging/prima/CORE/HDD/inc/wlan_hdd_cfg.h
@@ -55,7 +55,7 @@
 #endif /* DHCP_SERVER_OFFLOAD */
 
 //Number of items that can be configured
-#define MAX_CFG_INI_ITEMS   512
+#define MAX_CFG_INI_ITEMS   530
 
 #ifdef SAP_AUTH_OFFLOAD
 /* 802.11 pre-share key length */
